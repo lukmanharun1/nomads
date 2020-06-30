6048b2798451deeaecfd29982a1e497876ef3e8c
lukman <nandes88.ni@gmail.com>
2020-06-30 16:11:37 +0700

    menambahkan checkout-success

--- resources/views/includes/navbar-alternate.blade.php
+++ resources/views/includes/navbar-alternate.blade.php
@@ -2,8 +2,8 @@
 <div class="container">
   <nav class="row navbar navbar-expand-lg navbar-light bg-white">
     <div class="navbar-nav ml-auto mr-auto sm-auto mr-lg-0 mr-md-auto">
-      <a href="index.html" class="navbar-brand">
-        <img src="frontend/image/logo.png" alt="logo">
+      <a href="{{route('home')}} " class="navbar-brand">
+        <img src="{{url('frontend/image/logo.png')}} " alt="logo">
       </a>
     </div>
     <ul class="navbar-nav mr-auto d-none d-lg-block">
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -56,7 +56,7 @@
               <div class="travel-country">INDONESIA</div>
               <div class="travel-location">DRETAN,BALI</div>
               <div class="travel-button mt-auto">
-                <a href="details.html" class="btn btn-travel-details px-4">
+                <a href="{{route('detail')}} " class="btn btn-travel-details px-4">
                   View Details
                 </a>
               </div>
@@ -69,7 +69,7 @@
               <div class="travel-country">INDONESIA</div>
               <div class="travel-location">BROMO,MALANG</div>
               <div class="travel-button mt-auto">
-                <a href="details.html" class="btn btn-travel-details px-4">
+                <a href="{{route('detail')}} " class="btn btn-travel-details px-4">
                   View Details
                 </a>
               </div>
@@ -82,7 +82,7 @@
               <div class="travel-country">INDONESIA</div>
               <div class="travel-location">NUSA PENINDA</div>
               <div class="travel-button mt-auto">
-                <a href="details.html" class="btn btn-travel-details px-4">
+                <a href="{{route('detail')}} " class="btn btn-travel-details px-4">
                   View Details
                 </a>
               </div>
@@ -95,7 +95,7 @@
               <div class="travel-country">MIDDLE EAST</div>
               <div class="travel-location">DUBAI</div>
               <div class="travel-button mt-auto">
-                <a href="details.html" class="btn btn-travel-details px-4">
+                <a href="{{route('detail')}} " class="btn btn-travel-details px-4">
                   View Details
                 </a>
               </div>
