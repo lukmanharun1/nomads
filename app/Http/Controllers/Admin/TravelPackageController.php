15798202e36699a8d331d14a01ea72f747efb6ff
lukman <nandes88.ni@gmail.com>
2020-07-01 21:11:35 +0700

    menambahkan fitur crud

--- app/Http/Controllers/Admin/TravelPackageController.php
+++ app/Http/Controllers/Admin/TravelPackageController.php
@@ -66,7 +66,10 @@ class TravelPackageController extends Controller
      */
     public function edit($id)
     {
-        //
+        $item = TravelPackage::findOrFail($id);
+        return view('pages.admin.travel-package.edit', [
+            'item' => $item
+        ]);
     }
 
     /**
@@ -76,9 +79,13 @@ class TravelPackageController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(TravelPackageRequest $request, $id)
     {
-        //
+        $data = $request->all();
+        $data['slug'] = Str::slug($request->title);
+        $item = TravelPackage::findOrFail($id);
+        $item->update($data);
+        return redirect()->route('travel-package.index');
     }
 
     /**
@@ -89,6 +96,8 @@ class TravelPackageController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = TravelPackage::findOrFail($id);
+        $item->delete();
+        return redirect()->route('travel-package.index');
     }
 }
